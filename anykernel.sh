923e1343ef21fb5704d6f018e9520fc82d4a4a7d
OLEG-XEP <papademon1977@gmail.com>
2021-04-27 09:08:37 +0300

    Fix Headers

--- anykernel.sh
+++ anykernel.sh
@@ -27,22 +27,22 @@ ramdisk_compression=auto;
 dump_boot;
 
 ui_print " "
-ui_print " Mounting system and vendor..."
+ui_print "Mounting system and vendor..."
 mount -o rw,remount /system
 mount -o rw,remount /vendor
 
 ui_print " "
-ui_print " Backuping /system/build.prop to /system/build.prop.bak..."
+ui_print "Backuping /system/build.prop to /system/build.prop.bak..."
 cp /system/build.prop /system/build.prop.bak
 
 ui_print " "
-ui_print " Change value in /system/build.prop for fix error on device boot..."
+ui_print "Change value in /system/build.prop for fix error on device boot..."
 sed -i 's/ro.treble.enabled=true/ro.treble.enabled=false/g' /system/build.prop
 
 
 #Install Magisk Module
 ui_print " "
-ui_print " Installing Magisk Module..."
+ui_print "Installing Magisk Module..."
 
 cp -rf AutoInsmodModules /data/adb/modules
 
@@ -51,15 +51,15 @@ if
     [ -f /tmp/anykernel/kernel-headers.tar.xz ]
 then
     ui_print " "
-    ui_print " Installing Kernel Headers..."
+    ui_print "Installing Kernel Headers..."
     UNAME=$(ls /tmp/anykernel/modules/system/lib/modules/)
     NH_SYSTEM=/data/local/nhsystem
     for NH_PATH in $(ls $NH_SYSTEM)
     do NH_LIB=$NH_PATH/usr/lib/modules
     rm -rf $NH_LIB/*
     cp -r /tmp/anykernel/modules/system/lib/modules/$UNAME $NH_LIB
-    mkdir -p $NH_LIB/build
-    tar -xJf /tmp/anykernel/kernel-headers.tar.xz $NH_LIB/build
+    mkdir -p $NH_LIB/$UNAME/build
+    busybox tar xf /tmp/anykernel/kernel-headers.tar.xz -C $NH_LIB/$UNAME/build
     done
 else
 sleep 0
