6e251d4ba93dcda019abd7b6a580ff4087093c63
OLEG-XEP <papademon1977@gmail.com>
2021-05-29 19:25:27 +0000

    Release

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=
+kernel.string=Immortal Kernel with Full NetHunter Support by @D_L_Neron Based on StormBreaker 4.9.241
 do.devicecheck=0
 do.modules=1
 do.systemless=1
@@ -26,37 +26,27 @@ ramdisk_compression=auto;
 ## AnyKernel install
 dump_boot;
 
+#Mounting
+ui_print " "
+ui_print " "
+ui_print "Mounting system and vendor partitions..."
+mount -o remount,rw /system
+mount -o remount,rw /vendor
+sleep 3
+
 #Remove Old Modules
 ui_print " "
 ui_print " "
 ui_print "Removing Old Modules..."
 rm -rf /system/lib/modules
 rm -rf /vendor/lib/modules
+sleep 3
 
 #Install Magisk Module
 ui_print " "
 ui_print " "
 ui_print "Installing Magisk Module..."
-
 cp -rf /tmp/anykernel/AutoInsmodModules /data/adb/modules
-
-#Kernel headers
-if
-    [ -f /tmp/anykernel/headers.zip ]
-then
-    ui_print " "
-    ui_print " "
-    ui_print "Installing Kernel Headers..."
-    UNAME=$(ls /tmp/anykernel/modules/system/lib/modules/)
-    NH_SYSTEM=/data/local/nhsystem/*
-    NH_LIB=$NH_SYSTEM/usr/lib/modules
-    rm -rf $NH_LIB/*
-    cp -r /tmp/anykernel/modules/system/lib/modules/$UNAME $NH_LIB
-    mkdir -p $NH_LIB/$UNAME/build
-    busybox tar xf /tmp/anykernel/headers.tar.xz -C $NH_LIB/$UNAME/build
-    done
-else
-sleep 0
-fi
+sleep 3
 
 write_boot;
